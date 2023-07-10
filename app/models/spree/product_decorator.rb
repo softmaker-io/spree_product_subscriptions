aad070ce7837d4fcb79a3e49ebbdc340b86dcf23
Wiechsa <szymon.sudull.dev@gmail.com>
2023-07-10 11:28:47 +0200

    Product decorator

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,15 +1,17 @@
-Spree::Product.class_eval do
-
-  has_many :subscriptions, through: :variants_including_master, source: :subscriptions, dependent: :restrict_with_error
-  has_many :product_subscription_frequencies, class_name: "Spree::ProductSubscriptionFrequency", dependent: :destroy
-  has_many :subscription_frequencies, through: :product_subscription_frequencies, dependent: :destroy
-
-  alias_attribute :subscribable, :is_subscribable
-
-  self.whitelisted_ransackable_attributes += %w( is_subscribable )
-
-  scope :subscribable, -> { where(subscribable: true) }
-
-  validates :subscription_frequencies, presence: true, if: :subscribable?
-
+module Spree
+  module ProductDecorator
+    def self.prepended(base)
+      base.has_many :subscriptions, through: :variants_including_master, source: :subscriptions, dependent: :restrict_with_error
+      base.has_many :product_subscription_frequencies, class_name: "Spree::ProductSubscriptionFrequency", dependent: :destroy
+      base.has_many :subscription_frequencies, through: :product_subscription_frequencies, dependent: :destroy
+    
+      base.alias_attribute :subscribable, :is_subscribable
+    
+      base.whitelisted_ransackable_attributes += %w( is_subscribable )
+    
+      base.scope :subscribable, -> { where(subscribable: true) }
+    
+      base.validates :subscription_frequencies, presence: true, if: :subscribable?
+    end
+  end
 end
