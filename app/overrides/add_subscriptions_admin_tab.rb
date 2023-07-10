b45d77c130cabb8cf508dea337316e0694325d99
Wiechsa <szymon.sudull.dev@gmail.com>
2023-07-10 13:53:15 +0200

    Subscriptions tab

--- app/overrides/add_subscriptions_admin_tab.rb
+++ app/overrides/add_subscriptions_admin_tab.rb
@@ -1,6 +1,7 @@
 Deface::Override.new(
-  virtual_path: 'spree/layouts/admin',
-  name: 'subscriptions_admin_tab',
-  insert_bottom: '#main-sidebar',
-  partial: 'spree/admin/shared/subscriptions_sidebar_menu'
+  virtual_path: "spree/admin/shared/_main_menu",
+  name: 'subscriptions_settings_admin',
+  insert_before: "ul#sidebarConfiguration",
+  partial: 'spree/admin/shared/subscriptions_sidebar_menu',
+  disabled: false
 )
